87be1ea0f892943ab23d1fd83622a1112f3b0265
Fede Hanson <hanson.federico@gmail.com>
2023-03-16 23:44:21 -0300

    perf: :zap: Improvements to productsRouter

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -10,8 +10,8 @@ router.get('/',async (req,res) =>{
     let limitedProducts = []
     if (limit) limitedProducts = activeProducts.slice(0,limit)
     res.status(activeProducts.status ? activeProducts.status : 200)
-       .send(`${JSON.stringify({status: activeProducts.status ? 'error'      : 'success', 
-                               payload: limit              ? limitedProducts : activeProducts})}`)
+       .json({status: activeProducts.status ? 'error'      : 'success', 
+                               payload: limit              ? limitedProducts : activeProducts})
 })
 
 router.get('/:pid',async (req,res) =>{
@@ -23,16 +23,16 @@ router.get('/:pid',async (req,res) =>{
     }
     if(product[0].status) activeProduct = product
     res.status(activeProduct.status ? activeProduct.status : 200)
-       .send(`${JSON.stringify({status: activeProduct.status ? 'error' : 'success', 
-                               payload: activeProduct})}`)
+       .json({status: activeProduct.status ? 'error' : 'success', 
+                               payload: activeProduct})
 })
 
 router.post('/', async (req,res) => {
     const product = req.body
     const response = await productManager.addProduct(product)
     res.status(response.status ? response.status : 201)
-       .send(`${JSON.stringify({status: response.status ? 'error' : 'success', 
-                               payload: response})}`)
+       .json({status: response.status ? 'error' : 'success', 
+                               payload: response})
 })
 
 router.put('/:pid',async (req,res) =>{
@@ -40,16 +40,16 @@ router.put('/:pid',async (req,res) =>{
     const changes = req.body
     const response = await productManager.updateProduct(id,changes)
     res.status(response.status ? response.status : 201)
-       .send(`${JSON.stringify({status: response.status ? 'error' : 'success', 
-                               payload: response})}`)
+       .json({status: response.status ? 'error' : 'success', 
+                               payload: response})
 })
 
 router.delete('/:pid',async (req,res) =>{
     const id = Number(req.params.pid)
     const response = await productManager.deleteProduct(id)
     res.status(response.status ? response.status : 200)
-       .send(`${JSON.stringify({status: response.status ? 'error' : 'success', 
-                               payload: response})}`)
+       .json({status: response.status ? 'error' : 'success', 
+                               payload: response})
 })
 
 export default router
