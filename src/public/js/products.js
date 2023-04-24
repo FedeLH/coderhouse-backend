a9eb2dc286b8561347996dd6e545705a22885132
Fede Hanson <hanson.federico@gmail.com>
2023-04-24 15:02:09 -0300

    feat: :zap: Add selected options

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,69 +1,66 @@
-// let url = window.location.href
-// let urlObj = new URL(url)
-// let searchParams = urlObj.searchParams
+let url = window.location.href
+let urlObj = new URL(url)
+let searchParams = urlObj.searchParams
+let sort = document.getElementById('sort')
+let category = document.getElementById('category')
+let state = document.getElementById('status')
+let cart
 
-// const select = document.getElementById("miSelect")
-// let opcionSeleccionada = "opcion2"
-// for (var i = 0; i < select.options.length; i++) {
-//   if (select.options[i].value === valor) {
-//     select.selectedIndex = i
-//     break
-//   }
-// }
+const findIndex = (valueToFind, options) => {
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].value === valueToFind) {
+            return i
+        }
+    }
+}
 
-// function detectarCambio() {
-//     opcionSeleccionada = select.options[select.selectedIndex].value
-// }
-// const parsingUrl = _ => {
-//     if (window.location.search) {
-//         return window.location.search
-//     } else {
-//         return false
-//     }
-// } 
+if (searchParams.has('sort')) {
+    sort.selectedIndex = findIndex(searchParams.get('sort'),sort.options)
+}
 
-// var miString = "Este es un ejemplo de string";
-// if (miString.includes("ejemplo")) {
-//   console.log("El string contiene la palabra 'ejemplo'");
-// } else {
-//   console.log("El string no contiene la palabra 'ejemplo'");
-// }
+if (searchParams.has('query')) {
+    queryObj = JSON.parse(searchParams.get('query'))
+    let valueStatus = queryObj.status ?? 'none'
+    state.selectedIndex = findIndex(valueStatus,state.options)
+    let valueCategory = queryObj.category ?? 'none'
+    category.selectedIndex = findIndex(valueCategory,category.options)
+}
 
-// const changeOptions = select => {
-//     const value = select.options[select.selectedIndex].value
-
-//     Si no posee el parametro lo tiene que setear
-//     if (!searchParams.has(select.name) && select.name === "query") {
-        
-//         Tengo que armar query
-        
-//     }
-    
-//     Si posee el param pero elegi none, tengo que borrarlo
-//     if (value === "none") {
-//         searchParams.delete(select.name)
-//     }
-    
-//     Tengo que setear
-
-//     if (searchParams.has(select.name)) {
-//         if (value === "none") {
-//             searchParams.delete(select.name)
-//         } else {
-//             if (select.id === "sort") {
-//                 searchParams.set(select.id,value)
-//             }
-//         }
-//     }
-//     let params = parsingUrl()
-//     if (params) {
-//         if (params.includes(select.id)) {
-//             console.log("Tengo que reemplazar el valor")
-//             console.log(window.location.href)
-//         }
-//         console.log("Tengo que sumar")
-//         console.log(window.location.href += "&"+select.id+"="+select.options[select.selectedIndex].value)
-//     }
-//     console.log("Tengo que iniciar")
-//     console.log(window.location.href+"?"+select.id+"="+select.options[select.selectedIndex].value)
-// }
+const changeOptions = select => {
+    let value = select.options[select.selectedIndex].value
+    if (searchParams.has(select.name)) {
+        if (value === "none") {
+            if (select.name === 'query') {
+                queryObj = JSON.parse(searchParams.get('query'))
+                delete queryObj[select.id]
+                if (Object.keys(queryObj).length > 0) {
+                    value = JSON.stringify(queryObj)
+                    searchParams.set(select.name,value)
+                } else {
+                    searchParams.delete('query')
+                }
+            } else {
+                searchParams.delete(select.name)
+            }
+        } else {
+            if (select.name === 'query') {
+                queryObj = JSON.parse(searchParams.get('query'))
+                queryObj = {...queryObj,[select.id]: value}
+                value = JSON.stringify(queryObj)
+            }
+            searchParams.set(select.name,value)
+        }
+    } else {
+        if (select.name === 'query') {
+            queryObj = JSON.parse(searchParams.get('query'))
+            queryObj = {...queryObj,[select.id]: value}
+            value = JSON.stringify(queryObj)
+        }
+        searchParams.set(select.name,value)
+    }
+    if (searchParams.toString().length > 0) {
+        window.location.href = urlObj.origin+urlObj.pathname+"?"+searchParams.toString()
+    } else {
+        window.location.href = console.log(urlObj.origin+urlObj.pathname)
+    }
+}
